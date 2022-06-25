96ce4f8235d4f49996df826dca4214d7a17aefea
SEONGJU KIM <wind.orca.pe@gmail.com>
2022-06-25 15:15:28 +0900

    viewBinding 추가

---
+++ app/src/main/java/com/gdg/chicpick/login/LoginActivity.kt
@@ -0,0 +1,7 @@
+package com.gdg.chicpick.login
+
+import androidx.appcompat.app.AppCompatActivity
+
+class LoginActivity : AppCompatActivity() {
+
+}
---
+++ app/src/main/java/com/gdg/chicpick/survey/SurveyActivity.kt
@@ -0,0 +1,6 @@
+package com.gdg.chicpick.survey
+
+import androidx.appcompat.app.AppCompatActivity
+
+class SurveyActivity : AppCompatActivity() {
+}
